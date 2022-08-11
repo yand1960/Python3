554473ea4677c9e06413b1d640e36b626a95c603
yand <yand@outlook.com>
2022-08-11 11:36:59 +0300

    YA

--- po_05_slices.py
+++ po_05_slices.py
@@ -7,3 +7,7 @@ print(s[-3:]) #ld!
 print(s[2:10:2]) #lo o
 print(s[::2]) #Hlo ol!
 print(s[::-1]) #!dlrow ,olleH
+
+#https://github.com/yand1960/Python3
+
+#Найти все слова, которые заканчиваются на цо
