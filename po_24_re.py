a27206ef6fe9f9998c7bd28d43c4e23ff3fee60f
yand <yand@outlook.com>
2022-09-21 23:51:41 +0300

    YA

--- po_24_re.py
+++ po_24_re.py
@@ -1,15 +1,48 @@
-passport = "12 34 567890"
+import re
 
-valid = True
+passport = "12 34 567890aaa"
 
-if len(passport) != 12:
-    valid = False
+# valid = True
+#
+# if len(passport) != 12:
+#     valid = False
+#
+# if passport[2] != " " or passport[5] != " ":
+#     valid = False
+#
+# for i in [0, 1, 3, 4, 6, 7, 8, 9, 10, 11]:
+#     if passport[i] < "0" or passport[i] > "9":
+#         valid = False
+#
+# print(valid)
 
-if passport[2] != " " or passport[5] != " ":
-    valid = False
+# \d - цифра, \D - НЕ цифра
+# {2, 3} - Ровно 2 или 3 раза
+# ^ - начало текста, $ - конец текста
+pattern = "^\d{2} \d{2} \d{6}$"
+result = re.match(pattern, passport)
+print(not result is None)
 
-for i in [0, 1, 3, 4, 6, 7, 8, 9, 10, 11]:
-    if passport[i] < "0" or passport[i] > "9":
-        valid = False
+text = """
+Мама мыла раму.
+Тетя  Даша тащила шпалу.
+Тетя Оля отправилась на море.
+Дядя Вася тащил шпалу.
+Пете 8 лет.
+Тётя Глаша тащила шпалу.
+Дядя Коля тащил сома из реки.
+"""
 
-print(valid)
+# Как зовут теть, которые тащили шпалу?
+# \s - пробельный символ, \S - непробельный символ
+# + - в любом количестве
+# [её] - е или ё
+# (...) - группа, которая нас интересует
+pattern = "Т[её]тя\s+(\S+) тащила шпалу"
+result = re.findall(pattern, text)
+print(result)
+
+# Как зовут всех, кто тащил шпалу? (9:45)
+pattern = "(\S+) тащила{0,1} шпалу"
+result = re.findall(pattern, text)
+print(result)
